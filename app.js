981bb95c68b809bd9aa2f3b947e52f2c01925290
Estefany Silfa <esilfa3@outlook.com>
2022-02-13 00:08:30 -0500

    logo dispaly funtion

--- app.js
+++ app.js
@@ -3,3 +3,25 @@ const inquirer = require("inquirer");
 const connection = require ('./utils/connection')
 const questions = require ("./utils/questions")
 const table = require("console.table");
+
+displayLogo()
+start();
+
+function displayLogo() {
+    console.log(
+        logo({
+            name: 'Employee Management System',
+            lineChars: 12,
+            padding: 2,
+            margin: 3,
+            borderColor: 'blue',
+            logoColor: 'purple',
+            textColor: 'white',
+        })
+        .render()
+        );
+    }
+    async function start(){
+        
+    }
+    
