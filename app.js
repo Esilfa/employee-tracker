7d46ee44d71769b112e652b187fd3b204b064420
Estefany Silfa <esilfa3@outlook.com>
2022-02-13 00:37:25 -0500

    add app user choices

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 
 const inquirer = require("inquirer");
-const connection = require ('./utils/connection')
-const questions = require ("./utils/questions")
+const connection = require('./utils/connection')
+const questions = require("./utils/questions")
 const table = require("console.table");
+const logo = require("asciiart-logo");
+const { addEmployee, addDepartmentQuestions } = require("./utils/questions");
 
 displayLogo()
 start();
@@ -18,10 +20,66 @@ function displayLogo() {
             logoColor: 'purple',
             textColor: 'white',
         })
-        .render()
-        );
+            .render()
+    );
+}
+async function start() {
+
+    const userChoice = await inquirer.prompt(questions.initialQuestion);
+    switch (userChoice.initial) {
+        case 'add an employee':
+            addEmployee();
+            break;
+        case 'Add a department':
+            addDepartmentQuestions();
+            break;
+        case "Add a role":
+            addNewRole();
+            break;
+        case "View departments":
+            printDepartments();
+            break;
+        case "View employees":
+            printEmployees();
+            break;
+        case "Update employee role":
+            updateRole();
+            break;
+        case "View all employees by manager":
+            employeesByManager();
+            break;
+        case "Remove employee":
+            rmEmployee();
+            break;
+        case "View all employees by department":
+            employeesByDepartment();
+            break;
+        case "View all roles":
+            printRoles();
+            break;
+        case "Remove roles":
+            rmRole();
+            break;
+        case "Quit":
+            connection.end();
+            break;
+        default:
+            connection.end();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
-    async function start(){
-        
-    }
-    
+
+}
+
