eac3a38ddefe645552d2ecae5f67caf7f88906ff
Estefany Silfa <esilfa3@outlook.com>
2022-02-14 01:07:32 -0500

    add seeds/roles

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,32 @@
-INSERT INTO courses (id, course_title, course_description, active)
-VALUES ();
+INSERT INTO department (department_name)
+VALUES
+('Executive'),
+('Finance'),
+('Marketing'),
+('Sales'),
+('Legal'),
+('IT'),
+('HR');
+INSERT into
+  roles (title, salary, department_id)
+  VALUES 
+  ('President', 600000, 12),
+  ('CEO', 550000, 12),
+  ('Executive' 500000, 12),
+  ('Director of HR', 450000, 12),
+  ('Director of Sales', 400000, 12),
+  ('Director of Marketing', 400000, 12),
+  ('Sales Director', 380000, 123),
+  ('Sales Manager', 370000, 123),
+  ('Sales Assistant', 350000, 123),
+  ('Computer Programmer', 340000 ,1234),
+  ('Lawyer', 330000, 12345),
+ ('Legal Assistant', 310000, 12345),
+  ('Accountant', 350000, 123456),
+  ('HR Manager', 300000,1234567),
+  ('Marketing Manager', 300000, 12345678),
+  ('Marketing Assistant', 250000, 12345678);
+INSERT into
+  employee (first_name, last_name, role_id, manager_id)
+
        
