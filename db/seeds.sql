ce76d27bd141344f1f27814043bc04035e0d742b
Estefany Silfa <esilfa3@outlook.com>
2022-02-15 21:32:29 -0500

    add employees names

--- db/seeds.sql
+++ db/seeds.sql
@@ -20,13 +20,30 @@ INSERT into
   ('Sales Manager', 370000, 123),
   ('Sales Assistant', 350000, 123),
   ('Computer Programmer', 340000 ,1234),
-  ('Lawyer', 330000, 12345),
- ('Legal Assistant', 310000, 12345),
-  ('Accountant', 350000, 123456),
-  ('HR Manager', 300000,1234567),
-  ('Marketing Manager', 300000, 12345678),
-  ('Marketing Assistant', 250000, 12345678);
+  ('Lawyer', 330000, 5234),
+ ('Legal Assistant', 310000, 5234),
+  ('Accountant', 350000, 6123),
+  ('HR Manager', 300000,7234),
+  ('Marketing Manager', 300000, 8234),
+  ('Marketing Assistant', 250000, 8234);
 INSERT into
-  employee (first_name, last_name, role_id, manager_id)
+  employee (last_name, first_name, role_id, manager_id)
+  VALUES
+  ('Smith', 'Ally', 1,1),  
+  ('Johnson', 'Alex', 2,1),
+  ('Williams', 'Lyla', 3,1),
+  ('Brown', 'Kylia',4,1),
+  ('Jones', 'Bryce', 5, 2),
+  ('Miller' , 'Daniela', 6,2),
+  ('Davis', 'Jacob', 7,2),
+  ('Garcia', 'Reily', 8,2),
+  ('Rodriguez', 'Micaela',9,2),
+  ('Wilson', 'Breonia', 10,3),
+  ('Martinez','Miranda',11,3),
+  ('Anderson', 'Madelyn', 12,3),
+  ('Taylor','Jase', 13,3),
+  ('Thomas', 'Jessica', 14,3),
+  ('Hernandez', 'Samuel',15,3);
+
 
        
